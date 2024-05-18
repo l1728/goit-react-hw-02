c24a10395e7b1a9810cb89cffeea3281e2d1c051
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-18 22:37:46 +0300

    step2_3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,56 @@ import './App.css';
 import Description from './Description/Description';
 import Options from './Options/Options';
 import Feedback from './Feedback/Feedback';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Notification from './Notification/Notification';
 
 const App = () => {
-  const [feedback] = useState({
+  // Використовуємо хук useState для оголошення стану feedback з початковими значеннями
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
+  // Використовуємо хук useEffect для завантаження збереженого стану з localStorage при першому рендері
+  useEffect(() => {
+    // Отримуємо збережений стан з localStorage
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback) {
+      // Якщо є збережений стан, оновлюємо стан компоненту
+      setFeedback(JSON.parse(savedFeedback));
+    }
+    // Порожній масив залежностей означає, що цей ефект виконається лише один раз при монтуванні компонента
+  }, []);
+
+  // Використовуємо хук useEffect для збереження стану feedback у localStorage при кожній його зміні
+  useEffect(() => {
+    // Зберігаємо стан у localStorage у вигляді JSON
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+    // Масив залежностей з feedback означає, що цей ефект виконається кожного разу при зміні стану feedback
+  }, [feedback]);
+
+  // Функція updateFeedback приймає тип фідбеку та оновлює відповідне значення в стані feedback
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      // Копіюємо попередній стан
+      ...prevFeedback,
+      // Збільшуємо значення відповідного типу фідбеку на 1
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  // Обчислюємо загальну кількість відгуків
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   return (
     <>
       <Description />
-      <Options />
-      <Feedback feedback={feedback} />
+      <Options realFeedback={updateFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
   );
 };
